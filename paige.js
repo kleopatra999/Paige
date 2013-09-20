75579aea7760fd5fd6dff6c0e09e43a242d5f73c
Alex Lee <aintaer@gmail.com>
2013-09-19 21:03:28 -0400

    Moving all Network tests into be.net

--- paige.js
+++
@@ -1,9 +0,0 @@
-exports.SignUp = require('./lib/signup');
-exports.Project = require('./lib/project');
-exports.Home = require('./lib/home');
-exports.Profile = require('./lib/profile');
-exports.Account = require('./lib/account');
-exports.Dashboard = require('./lib/dashboard');
-exports.Login = require('./lib/login');
-
-exports.Helpers = require('./lib/helpers');
